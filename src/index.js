ae36a739f095f62fde579e38174c157531781a30
claudin <claudioberroeta@gmail.com>
2022-02-26 05:59:00 -0300

    Update index

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import AudioPlayer   from '@/components/player.vue'
 import AudioRecorder from '@/components/recorder.vue'
 
-const components = {
-  AudioPlayer,
-  AudioRecorder,
-
-  install (Vue) {
-    if (this.installed) {
-      return
-    }
-
-    this.installed = true
-
-    Vue.prototype.$eventBus = Vue.prototype.$eventBus || new Vue
-
-    Vue.component('audio-player', AudioPlayer)
-    Vue.component('audio-recorder', AudioRecorder)
+const plugin = {
+  install (instance)  {
+    instance.component('audio-player', AudioPlayer)
+    instance.component('audio-recorder', AudioRecorder)
   }
 }
 
-export default components
+export default plugin
 
 export { AudioPlayer, AudioRecorder }
